6345c30a9db2b5917da1d1996b3e956dd2eab7ed
HarromPS <anirudhsharma0192837465@gmail.com>
2024-05-08 22:55:24 +0530

    4th commit 1

--- cl_3/google/build_files.sh
+++ cl_3/google/build_files.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 echo "BUILD START"
 
 # create a virtual environment named 'venv' if it doesn't already exist
@@ -8,9 +6,9 @@ python3.9 -m venv venv
 # activate the virtual environment
 source venv/bin/activate
 
-# install Django and python-decouple in the virtual environment
-pip install django
-pip install python-decouple
+# install all deps in the venv
+pip install social-auth-app-django==5.4.1
+pip install -r requirements.txt
 
 # collect static files using the Python interpreter from venv
 python manage.py collectstatic --noinput
@@ -19,4 +17,5 @@ echo "BUILD END"
 
 # [optional] Start the application here 
 # python manage.py runserver
+
 
